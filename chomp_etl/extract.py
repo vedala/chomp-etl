bd589e3a7c8ff79ebbb68261eb61dbcec564cb2e
Sundaram Vedala <svedala23@gmail.com>
2018-07-25 12:04:36 -0400

    Check number of command-line arguments

--- chomp_etl/extract.py
+++ chomp_etl/extract.py
@@ -6,4 +6,12 @@ def main(argv):
     return my_dict
 
 if __name__ == "__main__":
+    num_args = len(sys.argv)
+    if num_args < 2:
+        print("Too few arguments. One argument expected.")
+        sys.exit(1)
+    elif num_args > 2:
+        print("Too many arguments. Only one argument expected.")
+        sys.exit(2)
+
     print(main(sys.argv[1]))
