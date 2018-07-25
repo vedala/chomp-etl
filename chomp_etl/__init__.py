ba4e2d08a9f655e5688f294dce32d0529b9bf8e7
Sundaram Vedala <svedala23@gmail.com>
2018-07-25 11:09:46 -0400

    Setup package files

---
+++ chomp_etl/__init__.py
@@ -0,0 +1,1 @@
+name = "chomp_etl"
---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="chomp_etl",
+    version="0.0.1",
+    author="Kalyan Vedala",
+    author_email="svedala23@gmail.com",
+    description="Chomp ETL",
+    long_description="A command-line ETL program.",
+    long_description_content_type="text/markdown",
+    url="https://github.com/vedala/chomp-etl",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+    ),
+)
