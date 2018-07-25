1859273dcd8dd8c67e2caf04fdea912bbd000a02
Sundaram Vedala <svedala23@gmail.com>
2018-07-25 12:32:45 -0400

    Refactor code in preparation for unit testing

--- chomp_etl/extract.py
+++ chomp_etl/extract.py
@@ -2,10 +2,6 @@ import sys
 import yaml
 
 def main(argv):
-    my_dict = yaml.load(open('example_job.yaml'))
-    return my_dict
-
-if __name__ == "__main__":
     num_args = len(sys.argv)
     if num_args < 2:
         print("Too few arguments. One argument expected.", file=sys.stderr)
@@ -14,4 +10,12 @@ if __name__ == "__main__":
         print("Too many arguments. Only one argument expected.", file=sys.stderr)
         sys.exit(2)
 
-    print(main(sys.argv[1]))
+    return read_yaml(argv[1])
+
+def read_yaml(filename):
+    my_dict = yaml.load(open('example_job.yaml'))
+    return my_dict
+
+
+if __name__ == "__main__":
+    print(main(sys.argv))
