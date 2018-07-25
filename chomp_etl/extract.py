2b6cc2f82f19ecd388a7eba23411101426f1081d
Sundaram Vedala <svedala23@gmail.com>
2018-07-25 13:06:16 -0400

    Refactor for better modularity

--- chomp_etl/extract.py
+++ chomp_etl/extract.py
@@ -2,7 +2,12 @@ import sys
 import yaml
 
 def main(argv):
-    num_args = len(sys.argv)
+    check_args(argv)
+    file_contents = get_file_contents(argv[1])
+    return yaml_str_to_dict(file_contents)
+
+def check_args(argv):
+    num_args = len(argv)
     if num_args < 2:
         print("Too few arguments. One argument expected.", file=sys.stderr)
         sys.exit(1)
@@ -10,12 +15,14 @@ def main(argv):
         print("Too many arguments. Only one argument expected.", file=sys.stderr)
         sys.exit(2)
 
-    return read_yaml(argv[1])
+def get_file_contents(filename):
+    with open(filename) as f:
+        contents = f.read()
+    return contents
 
-def read_yaml(filename):
-    my_dict = yaml.load(open(filename))
+def yaml_str_to_dict(yaml_str):
+    my_dict = yaml.load(yaml_str)
     return my_dict
-
 
 if __name__ == "__main__":
     print(main(sys.argv))
