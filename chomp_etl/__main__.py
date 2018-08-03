462cfa1183eff1550b5b5182b51e0df96fd67a8e
Sundaram Vedala <svedala23@gmail.com>
2018-08-03 14:45:46 -0400

    Remove coding and add comment

--- chomp_etl/__main__.py
+++ chomp_etl/__main__.py
@@ -1,4 +1,7 @@
-# -*- coding: utf-8 -*-
+"""
+    If the package is called as a script, execute the main()
+    function from chompetl.py.
+"""
 
 if __name__ == '__main__':
     from .chompetl import main
