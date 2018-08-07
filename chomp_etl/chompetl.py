1a34adc6ba0869ae597b8f68546f2ba174ba4cc7
Sundaram Vedala <svedala23@gmail.com>
2018-08-07 11:46:06 -0400

    Reorganize so sources are separate packages

--- chomp_etl/chompetl.py
+++ chomp_etl/chompetl.py
@@ -8,22 +8,27 @@ def main():
         sys.exit(1)
 
     try:
-        file_contents = get_file_contents(argv[1])
+        source_type = argv[1]
+        credentials = get_file_contents(argv[2])
+        source_config_json = get_file_contents(argv[3])
     except FileNotFoundError:
         sys.exit(2)
 
-    extract_config_dict = get_extract_config(file_contents)
-    normalized_extract_dict = normalize_extract_dict(extract_config_dict)
-    extract.extract_data(normalized_extract_dict)
+    source_config = json.loads(source_config_json)
+    extract_location = argv[4]
+    extract_filename = argv[5]
+    extract.extract(source_type, credentials, source_config,
+                                          extract_location, extract_filename)
 
 def check_args(argv):
     num_args = len(argv)
-    if num_args < 2:
-        print("Too few arguments. One argument expected.", file=sys.stderr)
-        return 1
-    elif num_args > 2:
-        print("Too many arguments. Only one argument expected.",
-                                                          file=sys.stderr)
+    if num_args != 6:
+        print("Incorrect number of arguments. Five arguments expected.",
+                                                        file=sys.stderr)
+        print(
+            f"Usage: {argv[0]} <source_type> <credentials_file> "
+            " <source_config_file> <extract_location> <extract_filename>",
+            file=sys.stderr)
         return 1
 
     return 0
@@ -37,21 +42,3 @@ def get_file_contents(filename):
         raise
 
     return contents
-
-def get_extract_config(config_file_contents):
-    my_dict = json.loads(config_file_contents)
-    return my_dict
-
-def normalize_extract_dict(extract_config_dict):
-    if extract_config_dict['extract_source'] == 'postgres':
-        tables_list = extract_config_dict['tables']
-        for table_dict in tables_list:
-            columns = get_column_list(table_dict['columns'])
-            table_dict['columns'] = columns
-    return extract_config_dict
-
-def get_column_list(columns_json_list):
-    column_list = []
-    for item in columns_json_list:
-        column_list.append(item['column_name'])
-    return column_list
--- chomp_etl/extract.py
+++ chomp_etl/extract.py
@@ -1,33 +1,30 @@
-import db_postgres
-import api_sendgrid
+import os
+import csv
+from source_postgres import SourcePostgres
 
-def extract_data(config_dict):
-    extract_source = config_dict['extract_source']
-    if extract_source == 'postgres':
-        extract_postgres(config_dict)
-    elif extract_source == 'sendgrid':
-        extract_sendgrid(config_dict)
+def extract(source_type, credentials, source_config,
+                                    extract_location, extract_filename):
+    if source_type == 'postgres':
+        source = SourcePostgres(credentials, source_config)
 
+    filename_with_path = construct_filename(extract_location, extract_filename)
+    file_obj = open(filename_with_path, 'w+')
+    while True:
+        data_batch = source.get_batch()
+        if len(data_batch) == 0:
+            break
+        write_batch(file_obj, data_batch)
 
-def extract_sendgrid(config_dict):
-    extract_location = config_dict['extract_folder']
-    api_key = config_dict['api_key']
-    fields = config_dict['stats']['fields']
-    start_date = config_dict['stats']['start_date']
-    end_date = config_dict['stats']['end_date']
-    api_sendgrid.fetch_and_write_data('stats', api_key, fields, start_date,
-                                            end_date, extract_location)
+    file_obj.close()
+    source.cleanup()
 
-def extract_postgres(config_dict):
-    extract_location = config_dict['extract_folder']
-    tables_list = config_dict['tables']
-    for table_dict in tables_list:
-        table_name = table_dict['name']
-        columns = table_dict['columns']
-        connect_string = table_dict['connect_string']
-        if 'fetch_rows' in table_dict:
-            num_fetch_rows = table_dict['fetch_rows']
-        else:
-            num_fetch_rows = None
-        db_postgres.fetch_and_write_data(table_name, columns,
-                            connect_string, extract_location, num_fetch_rows)
+def write_batch(file_obj, data_batch):
+    csv_writer = csv.writer(file_obj, quotechar='"',
+                                      quoting=csv.QUOTE_NONNUMERIC)
+    for row in data_batch:
+        csv_writer.writerow(row)
+
+def construct_filename(directory, base_filename):
+    filename_with_path = os.path.join(os.path.expanduser(directory),
+                                               base_filename + ".csv")
+    return filename_with_path
